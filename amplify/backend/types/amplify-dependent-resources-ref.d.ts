bf390ace042dc99a843a6e50528c8390e4e77efc
Rick Conrey <rick.conrey@witiproducts.com>
2021-11-20 14:36:41 -0800

    feat: add model and amplify files.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "kazucd76765b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "kazu": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "kazustorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
---
+++ lib/models/User.dart
@@ -0,0 +1,196 @@
+/*
+* Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+// NOTE: This file is generated and may not follow lint rules defined in your app
+// Generated files can be excluded from analysis in analysis_options.yaml
+// For more info, see: https://dart.dev/guides/language/analysis-options#excluding-code-from-analysis
+
+// ignore_for_file: public_member_api_docs, file_names, unnecessary_new, prefer_if_null_operators, prefer_const_constructors, slash_for_doc_comments, annotate_overrides, non_constant_identifier_names, unnecessary_string_interpolations, prefer_adjacent_string_concatenation, unnecessary_const, dead_code
+
+import 'package:amplify_datastore_plugin_interface/amplify_datastore_plugin_interface.dart';
+import 'package:flutter/foundation.dart';
+
+
+/** This is an auto generated class representing the User type in your schema. */
+@immutable
+class User extends Model {
+  static const classType = const _UserModelType();
+  final String id;
+  final String? _username;
+  final String? _email;
+  final String? _avatarKey;
+  final String? _description;
+  final TemporalDateTime? _dateCreated;
+
+  @override
+  getInstanceType() => classType;
+  
+  @override
+  String getId() {
+    return id;
+  }
+  
+  String? get username {
+    return _username;
+  }
+  
+  String? get email {
+    return _email;
+  }
+  
+  String? get avatarKey {
+    return _avatarKey;
+  }
+  
+  String? get description {
+    return _description;
+  }
+  
+  TemporalDateTime? get dateCreated {
+    return _dateCreated;
+  }
+  
+  const User._internal({required this.id, username, email, avatarKey, description, dateCreated}): _username = username, _email = email, _avatarKey = avatarKey, _description = description, _dateCreated = dateCreated;
+  
+  factory User({String? id, String? username, String? email, String? avatarKey, String? description, TemporalDateTime? dateCreated}) {
+    return User._internal(
+      id: id == null ? UUID.getUUID() : id,
+      username: username,
+      email: email,
+      avatarKey: avatarKey,
+      description: description,
+      dateCreated: dateCreated);
+  }
+  
+  bool equals(Object other) {
+    return this == other;
+  }
+  
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is User &&
+      id == other.id &&
+      _username == other._username &&
+      _email == other._email &&
+      _avatarKey == other._avatarKey &&
+      _description == other._description &&
+      _dateCreated == other._dateCreated;
+  }
+  
+  @override
+  int get hashCode => toString().hashCode;
+  
+  @override
+  String toString() {
+    var buffer = new StringBuffer();
+    
+    buffer.write("User {");
+    buffer.write("id=" + "$id" + ", ");
+    buffer.write("username=" + "$_username" + ", ");
+    buffer.write("email=" + "$_email" + ", ");
+    buffer.write("avatarKey=" + "$_avatarKey" + ", ");
+    buffer.write("description=" + "$_description" + ", ");
+    buffer.write("dateCreated=" + (_dateCreated != null ? _dateCreated!.format() : "null"));
+    buffer.write("}");
+    
+    return buffer.toString();
+  }
+  
+  User copyWith({String? id, String? username, String? email, String? avatarKey, String? description, TemporalDateTime? dateCreated}) {
+    return User(
+      id: id ?? this.id,
+      username: username ?? this.username,
+      email: email ?? this.email,
+      avatarKey: avatarKey ?? this.avatarKey,
+      description: description ?? this.description,
+      dateCreated: dateCreated ?? this.dateCreated);
+  }
+  
+  User.fromJson(Map<String, dynamic> json)  
+    : id = json['id'],
+      _username = json['username'],
+      _email = json['email'],
+      _avatarKey = json['avatarKey'],
+      _description = json['description'],
+      _dateCreated = json['dateCreated'] != null ? TemporalDateTime.fromString(json['dateCreated']) : null;
+  
+  Map<String, dynamic> toJson() => {
+    'id': id, 'username': _username, 'email': _email, 'avatarKey': _avatarKey, 'description': _description, 'dateCreated': _dateCreated?.format()
+  };
+
+  static final QueryField ID = QueryField(fieldName: "user.id");
+  static final QueryField USERNAME = QueryField(fieldName: "username");
+  static final QueryField EMAIL = QueryField(fieldName: "email");
+  static final QueryField AVATARKEY = QueryField(fieldName: "avatarKey");
+  static final QueryField DESCRIPTION = QueryField(fieldName: "description");
+  static final QueryField DATECREATED = QueryField(fieldName: "dateCreated");
+  static var schema = Model.defineSchema(define: (ModelSchemaDefinition modelSchemaDefinition) {
+    modelSchemaDefinition.name = "User";
+    modelSchemaDefinition.pluralName = "Users";
+    
+    modelSchemaDefinition.authRules = [
+      AuthRule(
+        authStrategy: AuthStrategy.PUBLIC,
+        operations: [
+          ModelOperation.CREATE,
+          ModelOperation.UPDATE,
+          ModelOperation.DELETE,
+          ModelOperation.READ
+        ])
+    ];
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.id());
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: User.USERNAME,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: User.EMAIL,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: User.AVATARKEY,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: User.DESCRIPTION,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: User.DATECREATED,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
+  });
+}
+
+class _UserModelType extends ModelType<User> {
+  const _UserModelType();
+  
+  @override
+  User fromJson(Map<String, dynamic> jsonData) {
+    return User.fromJson(jsonData);
+  }
+}
