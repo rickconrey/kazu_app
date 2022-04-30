98fbfa97139811d286600ad12e21c189170ede69
Rick Conrey <rick.conrey@witiproducts.com>
2022-04-29 17:39:15 -0700

    fix: cleanup unused code.

--- lib/repositories/ble_repository.dart
+++ lib/repositories/ble_repository.dart
@@ -5,20 +5,8 @@ import 'package:kazu_app/utils/packet.dart';
 import 'package:synchronized/synchronized.dart';
 
 class BleRepository {
-  //final FlutterBlue ble = FlutterBlue.instance;
   final ble = FlutterReactiveBle();
   ProcessPacket processPacket = ProcessPacket();
-
-//  Future<void> scanForDevices(Duration? timeout) async {
-//    try {
-//    //  var scanStream = ble.scan(timeout: const Duration(seconds: 4)).listen((result) {
-//    //    BleScanResult(result: result);
-//    //  });
-//
-//    } catch (e) {
-//      rethrow;
-//    }
-//  }
 
   Stream<DiscoveredDevice> scan(List<Uuid> services) {
     return ble.scanForDevices(withServices: services, scanMode: ScanMode.balanced);
@@ -48,56 +36,23 @@ class BleRepository {
     return ble.subscribeToCharacteristic(qc);
   }
 
-  //StreamBuilder<List<ScanResult> scanResults() {
-  //  return ble.scanResults;
-  //}
-
-//  Future<void> stopScan() async {
-//    await ble.stopScan();
-//  }
-//
-//  Future<void> setNotificationValue(BluetoothCharacteristic characteristic, bool value) async {
-//   await characteristic.setNotifyValue(value);
-//  }
-//
-//  Future<void> txMessage(BluetoothCharacteristic characteristic, List<int> message) async {
-//    await characteristic.write(message);
-//  }
-//
-//  Future<List<int>> rxMessage(BluetoothCharacteristic characteristic) async {
-//    return await characteristic.read();
-//  }
-//
-//  Future<void> connectToDevice(BluetoothDevice device) async {
-//    try {
-//      await device.connect(timeout: const Duration(seconds: 4), autoConnect: true);
-//    } catch (e) {
-//      rethrow;
-//    }
-//  }
-//
   Future<void> disconnectFromDevice(StreamSubscription<ConnectionStateUpdate> connection) async {
     await connection.cancel();
   }
 
   Future<PacketResult?> readFromDevice(QualifiedCharacteristic rx, Lock lock) async {
-    //if(rx.properties.read) {
-      print("Reading data");
-      List<int> values = [];
-      await lock.synchronized(() async {
-        //values = await rx.read();
-        values = await read(qc: rx);
-      });
-      print(values);
-      if (values.length > 4) {
-        Packet packet = Packet();
-        packet.processRx(values);
-        PacketResult? results = processPacket.processPacket(packet);
-        return results;
-      }
-    //} else {
-    //  print("No read property found.");
-    //}
+    print("Reading data");
+    List<int> values = [];
+    await lock.synchronized(() async {
+      values = await read(qc: rx);
+    });
+    print(values);
+    if (values.length > 4) {
+      Packet packet = Packet();
+      packet.processRx(values);
+      PacketResult? results = processPacket.processPacket(packet);
+      return results;
+    }
     return null;
   }
 }
